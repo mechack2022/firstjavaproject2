60267582cc439d8614a9303c431a563d87c546c6
Decagon <decagon@Decagons-MacBook-Air.local>
2022-11-24 18:11:07 +0100

    recursive java code

--- src/Recursive/Recursive.java
+++ src/Recursive/Recursive.java
@@ -1,4 +1,15 @@
 package Recursive;
 
+import java.util.ArrayList;
+
 public class Recursive {
+
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+    public int sumlist(int el){
+        if(numbers.length == 1){
+            return numbers[0];
+        }
+        sumlist()
+    }
 }
